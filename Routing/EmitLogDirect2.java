81ce249c803c4f72544f9d5528179faca1ea812c
JobWOD <job.jdrh@gmail.com>
2016-04-28 15:45:10 -0500

    Sensores implementados como hilos

--- Routing/EmitLogDirect2.java
+++ Routing/EmitLogDirect2.java
@@ -1,7 +1,7 @@
 import com.rabbitmq.client.*;
 import java.io.IOException;
 
-public class EmitLogDirect extends Thread implements Runnable{
+public class EmitLogDirect2 extends Thread implements Runnable{
 
   private static final String EXCHANGE_NAME = "direct_logs";
   private static ConnectionFactory factory;
@@ -15,11 +15,11 @@ public class EmitLogDirect extends Thread implements Runnable{
     if (i == 0){
       try{
         Thread.sleep(2000);
-        enviaM("A","HolaD");
+        enviaM("B","HolaD");
       }
       catch(Exception e){}
     }
-      
+    
     Consumer consumer = new DefaultConsumer(channel) {
       @Override
       public void handleDelivery(String consumerTag, Envelope envelope,
@@ -36,15 +36,15 @@ public class EmitLogDirect extends Thread implements Runnable{
       channel = connection.createChannel();
       channel.exchangeDeclare(EXCHANGE_NAME, "direct");
       queueName = channel.queueDeclare().getQueue();
-      channel.queueBind(queueName, EXCHANGE_NAME, "AA");
-
+      channel.queueBind(queueName, EXCHANGE_NAME, "BB");
+      
       channel.basicConsume(queueName, true, consumer);
 
     }
     catch(Exception e){}
     
   }
-
+  
   public static void enviaM (String id, String message) throws Exception {
     factory = new ConnectionFactory();
     factory.setHost("localhost");
@@ -52,7 +52,7 @@ public class EmitLogDirect extends Thread implements Runnable{
     channel = connection.createChannel();
     channel.exchangeDeclare(EXCHANGE_NAME, "direct");
     queueName = channel.queueDeclare().getQueue();
-    channel.queueBind(queueName, EXCHANGE_NAME, "AA");
+    channel.queueBind(queueName, EXCHANGE_NAME, "BB");
 
     channel.basicPublish(EXCHANGE_NAME, id, null, message.getBytes("UTF-8"));
     System.out.println(" [x] Sent '" + id + "':'" + message + "'");
--- Routing/ReceiveLogsDirect.java
+++ Routing/ReceiveLogsDirect.java
@@ -1,13 +1,13 @@
 import com.rabbitmq.client.*;
 import java.io.IOException;
 
-public class ReceiveLogsDirect {
+public class ReceiveLogsDirect{
 
   private static final String EXCHANGE_NAME = "direct_logs";
   private static ConnectionFactory factory;
   private static Connection connection;
   private static Channel channel;
-  private static String queueName;
+  private static String queueName,queue2;
 
   public static void main(String[] argv) throws Exception {
     factory = new ConnectionFactory();
@@ -16,7 +16,10 @@ public class ReceiveLogsDirect {
     channel = connection.createChannel();
     channel.exchangeDeclare(EXCHANGE_NAME, "direct");
     queueName = channel.queueDeclare().getQueue();
-    channel.queueBind(queueName, EXCHANGE_NAME, "M");
+    channel.queueBind(queueName, EXCHANGE_NAME, "A");
+
+    queue2 = channel.queueDeclare().getQueue();
+    channel.queueBind(queue2, EXCHANGE_NAME, "B");
     
     System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
@@ -27,18 +30,20 @@ public class ReceiveLogsDirect {
         String message = new String(body, "UTF-8");
         System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
         try{
-          enviaM();
+          if (envelope.getRoutingKey().equals("A"))
+            enviaM("AA","HolaA");
+          if (envelope.getRoutingKey().equals("B"))
+            enviaM("BB","HolaB");
         }
         catch(Exception e){}
       }
     };
     channel.basicConsume(queueName, true, consumer);
+    channel.basicConsume(queue2, true, consumer); 
   }
 
-  public static void enviaM () throws Exception {
-    String id = "H";
-    String message = "Hola";
-
+  public static void enviaM (String id, String message) throws Exception {
+    
     channel.basicPublish(EXCHANGE_NAME, id, null, message.getBytes("UTF-8"));
     System.out.println(" [x] Sent '" + id + "':'" + message + "'");
   }
